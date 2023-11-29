37cdf375c05b850c74cc3fd0638f75292c6a87a5
Hamin Lee <whereanna7@gmail.com>
2023-11-29 17:28:36 +0900

    임시 로그인 state 생성 후, state에 따른 페이지 접근 컨트롤 추가

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-function Auth() {
+function Auth({isLogin, setIsLogin}) {
+  const navigate = useNavigate();
   return (
-    <div>Auth</div>
+    <div>
+      <button onClick={()=> {
+        setIsLogin(true);
+        alert("로그인 완료");
+      }}>로그인하기</button>
+      <button onClick={()=>{
+        setIsLogin(false);
+        alert("로그아웃 완료");
+      }}>로그아웃하기</button> 
+      <button onClick={()=> {navigate("/")}}>홈</button>
+      <button onClick={()=> {navigate("/letter-details/:id")}}>상세페이지</button>
+      <button onClick={()=> {navigate("/profile")}}>프로필</button>
+      <br /> 로그인 여부: {String(isLogin)}
+    </div>
   )
 }
 
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,20 +1,29 @@
 // import React, { useEffect, useState } from 'react'
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import LetterDetails from "../pages/LetterDetails";
 import Profile from "pages/Profile";
 import Auth from "pages/Auth";
 
 function Router() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="auth" element={<Auth />} />
-        <Route path="/" element={<Home />} />
-        <Route path="letter-details/:id" element={<LetterDetails />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="*" element={<Home />} />
+        <Route
+          path="auth/*"
+          element={<Auth isLogin={isLogin} setIsLogin={setIsLogin} />}
+        />
+        {isLogin ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="letter-details/:id" element={<LetterDetails />} />
+            <Route path="profile" element={<Profile />} />
+          </>
+        ) : (
+          <Route path="*" element={<Navigate to="/auth" replace />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
