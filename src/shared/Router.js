fce8e94d4d48ef8dab02ecabaafb1c380f9125a8
Hamin Lee <whereanna7@gmail.com>
2023-11-29 16:35:32 +0900

    로그인, 프로필 페이지 추가 및 router 설정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,20 +1,23 @@
 // import React, { useEffect, useState } from 'react'
-import React  from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from '../pages/Home';
-import LetterDetails from '../pages/LetterDetails';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import LetterDetails from "../pages/LetterDetails";
+import Profile from "pages/Profile";
+import Auth from "pages/Auth";
 
 function Router() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="letter-details/:id" element={<LetterDetails />}/>
+        <Route path="auth" element={<Auth />} />
+        <Route path="/" element={<Home />} />
+        <Route path="letter-details/:id" element={<LetterDetails />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default Router
+export default Router;
